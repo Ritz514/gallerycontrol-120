37157a442ed12112fa0afa210a2cf9baab614210
Ritz514 <142646304+Ritz514@users.noreply.github.com>
2024-04-29 08:31:54 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/gallerycontrol/MainActivity.java
+++ app/src/main/java/com/example/gallerycontrol/MainActivity.java
@@ -1,14 +1,52 @@
-package com.example.gallerycontrol;
+package com.example.gallery_control;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Gallery simpleGallery;
+    CustomizedGalleryAdapter customGalleryAdapter;
+    ImageView selectedImageView;
+
+    // To show the selected language, we need this
+    // array of images, here taken 10 different kind of
+    // most popular programming languages
+    int[] images = {
+            R.drawable.india,
+            R.drawable.china,
+            R.drawable.australia,
+            R.drawable.america
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Our layout is activity_main
+        // get the reference of Gallery. As we are showing
+        // languages it is named as languagesGallery
+        // meaningful names will be good for easier understanding
+        simpleGallery = (Gallery) findViewById(R.id.languagesGallery);
+
+        // get the reference of ImageView
+        selectedImageView = (ImageView) findViewById(R.id.imageView);
+
+        // initialize the adapter
+        customGalleryAdapter = new CustomizedGalleryAdapter(getApplicationContext(), images);
+
+        // set the adapter for gallery
+        simpleGallery.setAdapter(customGalleryAdapter);
+
+        // Let us do item click of gallery and image can be identified by its position
+        simpleGallery.setOnItemClickListener((parent, view, position, id) -> {
+            // Whichever image is clicked, that is set in the selectedImageView
+            // position will indicate the location of image
+            selectedImageView.setImageResource(images[position]);
+        });
     }
 }
